de5b37c24821e72e6b92d8c1e213f18885c0247e
Austin Dmello <austindmello2004@outlook.com>
2024-09-17 20:54:43 +0530

    new updated two

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -24,7 +24,7 @@ const MobileNav = () => {
                 </SheetTrigger>
                 <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
                     <Image
-                        src="/assets/images/logo.svg"
+                        src="/assets/images/logos.svg"
                         alt="logo"
                         width={128}
                         height={38}
