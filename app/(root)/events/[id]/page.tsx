31e0f2eb7ef0831b9f4601b64b3e94b64ee8a5d4
Austin Dmello <austindmello2004@outlook.com>
2024-10-06 17:20:42 +0530

    implement stripe

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CheckoutButton from "@/components/shared/CheckoutButton";
 import Collection from "@/components/shared/Collection";
 import { getEventById, getRelatedEventsByCategory } from "@/lib/actions/event.actions"
 import { formatDateTime } from "@/lib/utils";
@@ -42,6 +43,7 @@ const EventDetails = async ({ params: { id }, searchParams}: SearchParamProps) =
                         </div>
                     </div>
                     {/* CHECKOUT BUTTON */}
+                    <CheckoutButton event={event}/>
                     <div className="flex flex-col gap-5">
                         <div className="flex gap-2 md:gap-3">
                             <Image src="/assets/icons/calendar.svg" alt="calendar"
