c6324aad0c9d34c0690eead4e62a8003eb399dc5
Austin Dmello <austindmello2004@outlook.com>
2024-10-06 22:49:38 +0530

    finish the app

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -55,7 +55,7 @@ const Card = ({event, hasOrderLink, hidePrice}: CardProps) => {
                             {event.organizer.firstName} {event.organizer.lastName}
                         </p>
                         {hasOrderLink && (
-                            <Link href={`/orders?eventId?=${event._id}`} className='flex gap-2'>
+                            <Link href={`/orders?eventId=${event._id}`} className='flex gap-2'>
                                 <p className="text-primary-500">Order Details</p>
                                 <Image src="/assets/icons/arrow.svg" alt="search" width={10} height={10}/>
                             </Link>
